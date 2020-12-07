19c29ff19a18e16513f5e454d2483a91dc8018a8
Julia1999000 <jyliu1999@gmail.com>
2020-12-07 22:21:52 +0300

    необходимый код и DockerFile

---
+++ src/main/java/com/web/Controller.java
@@ -0,0 +1,33 @@
+package com.web;
+
+import com.model.News;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+
+    private List<News> inf = new ArrayList<>();
+
+    @PostConstruct
+    private void init() {
+        for (int i = 0; i < 5; i++) {
+            inf.add(News.builder()
+                    .title("My title" + i)
+                    .description("My description" + i)
+                    .build());
+        }
+    }
+
+    @GetMapping("/news")
+    public List<News> getLists() {
+        return inf;
+    }
+
+}
